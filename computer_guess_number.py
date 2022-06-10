d7ba48f9c01fab7c46a5ea61bba2d5ebe74249e2
Akeeb Khandakar <90944924+akeeb2002@users.noreply.github.com>
2022-06-10 16:39:33 -0400

    Create computer_guess_number.py

---
+++ computer_guess_number.py
@@ -0,0 +1,22 @@
+import random
+
+
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ''
+    while feedback != 'c':
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f"Is the {guess} too high(H), too low(L) or correct(C)?").lower()
+        if feedback == 'h':
+            high = guess -1
+        elif feedback == 'l':
+            low = guess + 1
+    
+    print('Good Job!')
+
+
+computer_guess(10)
